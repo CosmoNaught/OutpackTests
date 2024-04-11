c5b4a3f6fcfa9da501814df9e60fe4914726bb39
Cosmo Santoni <santonicosmo@gmail.com>
2024-04-11 13:34:08 +0100

    remove redundancies

--- src/figs/figs.py
+++ src/figs/figs.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import orderly
 import pandas as pd
+import os
+import sys
 from plot import plot_distributions_from_csv
 
 def main():
-    print("Running...")
+
     data = pd.read_csv("data.csv")
 
     plot_distributions_from_csv(data)
